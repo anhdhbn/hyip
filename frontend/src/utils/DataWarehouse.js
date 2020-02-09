af327be9c855d2656152594e3b501afa9ff27a1c
Anh DH <anhdhbn@gmail.com>
2020-02-09 07:49:00 +0700

    finish project details chart

---
+++ frontend/src/utils/DataWarehouse.js
@@ -0,0 +1,149 @@
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities'
+
+import crawldataServices from '../services/crawldata'
+
+const brandPrimary = getStyle('--primary')
+const brandSuccess = getStyle('--success')
+// const brandInfo = getStyle('--info')
+const brandWarning = getStyle('--warning')
+const brandDanger = getStyle('--danger')
+
+
+
+const optionsChart = {
+  tooltips: {
+    enabled: false,
+    custom: CustomTooltips,
+    intersect: true,
+    mode: 'index',
+    position: 'nearest',
+    callbacks: {
+      labelColor: function(tooltipItem, chart) {
+        return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].borderColor }
+      }
+    }
+  },
+  maintainAspectRatio: false,
+  // legend: {
+  //   display: false,
+  // },
+  elements: {
+    point: {
+      radius: 3,
+      hitRadius: 10,
+      hoverRadius: 4,
+      hoverBorderWidth: 3,
+    },
+  },
+}
+
+const createDataForChart = (labelName, data, color) => {
+  return {
+    label: labelName,
+    backgroundColor: hexToRgba(color, 10),
+    borderColor: color,
+    pointHoverBackgroundColor: '#fff',
+    borderWidth: 2,
+    // fill: false,
+    // lineTension: 0.1,
+    // borderCapStyle: 'butt',
+    // borderDash: [],
+    // borderDashOffset: 0.0,
+    // borderJoinStyle: 'miter',
+    // pointBorderColor: color,
+    // pointBackgroundColor: '#fff',
+    // pointBorderWidth: 1,
+    // pointHoverRadius: 10,
+    // pointHoverBackgroundColor: color,
+    // pointHoverBorderColor: 'rgba(220,220,220,1)',
+    // pointHoverBorderWidth: 2,
+    // pointRadius: 3,
+    // pointHitRadius: 10,
+    data: data,
+  }
+};
+
+const calcProfit = (total_investment, total_paid_out) => {
+  let d1 = total_investment
+  let d2 = total_paid_out
+  let profit = []
+  for(let i=0; i< d1.length; i++){
+    profit.push(d1[i] - d2[i])
+  }
+  return profit
+}
+
+const getProfitData = (total_investment, total_paid_out, labels) => {
+  let profit = calcProfit(total_investment, total_paid_out)
+  return {
+    labels: labels,
+    datasets: [
+      createDataForChart('Total investment', total_investment, brandPrimary),
+      createDataForChart('Total paid out', total_paid_out, brandWarning),
+      createDataForChart('Profit', profit, brandSuccess),
+    ]
+  }
+}
+
+const getGrowthRateData = (total_investment, total_paid_out, labels) => {
+  let profit = calcProfit(total_investment, total_paid_out)
+
+  let  igr = []
+  for(let i=1; i< profit.length; i++){
+    if (profit[i]  === 0) {
+      igr.push(0)
+    } else {
+      let item = (profit[i] - profit[i-1])/profit[i]
+      igr.push(item)
+    }
+  }
+  return {
+    labels: labels.reverse().slice(1).reverse(),
+    datasets: [
+      createDataForChart('Income Growth Rate', igr, brandDanger),
+    ]
+  }
+}
+
+const createSingleData = (labels, label, data) => {
+  return {
+    labels: labels,
+    datasets: [
+      createDataForChart(label, data, brandPrimary),
+    ]
+  }
+}
+
+const preprocessFetchData = (id, days='all') => {
+  return new Promise((resolve, reject) => {
+    crawldataServices.fetchDataCrawledProject(id, days).then(res => {
+      res.drawData = {}
+      let data = res.data
+      let dates = data.map(e => new Date(e.created_date))
+      let labels = dates.map(e => `${e.getDate()}/${e.getMonth()+1}`)
+      let total_investment = data.map(e => e.total_investment)
+      let total_paid_out = data.map(e => e.total_paid_out)
+      let total_members = data.map(e => e.total_member)
+      let alexa_rank = data.map(e => e.alexa_rank)
+      
+      res.drawData.labels = labels
+      res.drawData.total_investment = total_investment
+      res.drawData.total_paid_out = total_paid_out
+      res.drawData.total_members = total_members
+      res.drawData.alexa_rank = alexa_rank
+      resolve(res)
+    })
+  })
+}
+
+export default {
+  optionsChart,
+  createDataForChart,
+  calcProfit,
+  getProfitData,
+  getProfitData,
+  createSingleData,
+  getGrowthRateData,
+  preprocessFetchData
+}
--- frontend/src/views/Widgets/ProjectWidget.js
+++ frontend/src/views/Widgets/ProjectWidget.js
@@ -1,29 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Button,
-  ButtonGroup,
-  ButtonToolbar,
   Card,
   CardBody,
   CardFooter,
   CardTitle,
   Col,
   Row,
+  Button,
+  ButtonGroup,
+  ButtonToolbar,
 } from "reactstrap";
 import { Line } from 'react-chartjs-2';
 import { parse } from 'url';
-import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
-import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities'
-
+import dataWarehouse from "../../utils/DataWarehouse"
 import projectServices from '../../services/projects'
-import crawldataServices from '../../services/crawldata'
-
-const brandPrimary = getStyle('--primary')
-const brandSuccess = getStyle('--success')
-// const brandInfo = getStyle('--info')
-const brandWarning = getStyle('--warning')
-const brandDanger = getStyle('--danger')
 
 const propTypes = {
   id: PropTypes.string,
@@ -33,50 +24,20 @@ const defaultProps = {
   id: null,
 };
 
-const options = {
-  tooltips: {
-    enabled: false,
-    custom: CustomTooltips,
-    intersect: true,
-    mode: 'index',
-    position: 'nearest',
-    callbacks: {
-      labelColor: function(tooltipItem, chart) {
-        return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].borderColor }
-      }
-    }
-  },
-  maintainAspectRatio: false,
-  legend: {
-    display: false,
-  },
-  elements: {
-    point: {
-      radius: 3,
-      hitRadius: 10,
-      hoverRadius: 4,
-      hoverBorderWidth: 3,
-    },
-  },
-}
-
-
 class ProjectWidget extends Component {
   constructor(props){
     super(props);
-    this.getPaidData = this.getPaidData.bind(this)
-    this.getGrowthRateData = this.getGrowthRateData.bind(this)
     this.fetchData = this.fetchData.bind(this)
     this.state = {
-      dataProject: {},
       crawldata: {},
       drawData: {
         labels: [],
         total_investment: [],
         total_paid_out: [],
-        total_member: [],
+        total_members: [],
         alexa_rank: [],
       },
+      dataProject: {},
       days: "all"
     }
   }
@@ -89,105 +50,24 @@ class ProjectWidget extends Component {
         this.setState({dataProject: {...this.state.dataProject, domain: domain}})
       }
     })
-    this.fetchData()
+
+    
+    this.fetchData(this.props.id)
   }
 
-  fetchData(days='all') {
+  fetchData(id, days='all') {
     this.setState({days: days})
-    crawldataServices.fetchDataCrawledProject(this.props.id, this.state.days).then(res => {
-      this.setState({...this.state, crawldata: res.data})
-
-      let data = this.state.crawldata
-      let dates = data.map(e => new Date(e.created_date))
-      let labels = dates.map(e => `${e.getDate()}/${e.getMonth()+1}`)
-      let total_investment = data.map(e => e.total_investment)
-      let total_paid_out = data.map(e => e.total_paid_out)
-      let total_member = data.map(e => e.total_member)
-      let alexa_rank = data.map(e => e.alexa_rank)
-
-      this.setState({drawData: {...this.state.drawData, labels: labels}})
-      this.setState({drawData: {...this.state.drawData, total_investment: total_investment}})
-      this.setState({drawData: {...this.state.drawData, total_paid_out: total_paid_out}})
-      this.setState({drawData: {...this.state.drawData, total_member: total_member}})
-      this.setState({drawData: {...this.state.drawData, alexa_rank: alexa_rank}})
+    dataWarehouse.preprocessFetchData(id, this.state.days).then(res=>{
+      this.setState({drawData: res.drawData, crawldata: res.data})
     })
   }
 
-  createDataForChart(labelName, data, color) {
-    return {
-      label: labelName,
-      backgroundColor: hexToRgba(color, 10),
-      borderColor: color,
-      pointHoverBackgroundColor: '#fff',
-      borderWidth: 2,
-      // fill: false,
-      // lineTension: 0.1,
-      // borderCapStyle: 'butt',
-      // borderDash: [],
-      // borderDashOffset: 0.0,
-      // borderJoinStyle: 'miter',
-      // pointBorderColor: color,
-      // pointBackgroundColor: '#fff',
-      // pointBorderWidth: 1,
-      // pointHoverRadius: 10,
-      // pointHoverBackgroundColor: color,
-      // pointHoverBorderColor: 'rgba(220,220,220,1)',
-      // pointHoverBorderWidth: 2,
-      // pointRadius: 3,
-      // pointHitRadius: 10,
-      data: data,
-    }
-  }
-
-  getPaidData() {
-    let d1 = this.state.drawData.total_investment
-    let d2 = this.state.drawData.total_paid_out
-    let profit = []
-    for(let i=0; i< d1.length; i++){
-      profit.push(d1[i] - d2[i])
-    }
-
-    return {
-      labels: this.state.drawData.labels,
-      datasets: [
-        this.createDataForChart('Total investment', this.state.drawData.total_investment, brandPrimary),
-        this.createDataForChart('Total paid out', this.state.drawData.total_paid_out, brandWarning),
-        this.createDataForChart('Profit', profit, brandSuccess),
-      ]
-    }
-  }
-
-  getGrowthRateData(){
-    let d1 = this.state.drawData.total_investment
-    let d2 = this.state.drawData.total_paid_out
-    let profit = []
-    for(let i=0; i< d1.length; i++){
-      profit.push(d1[i] - d2[i])
-    }
-
-    let  igr = []
-    for(let i=1; i< profit.length; i++){
-      if (profit[i]  === 0) {
-        igr.push(0)
-      } else {
-        let item = (profit[i] - profit[i-1])/profit[i]
-        igr.push(item)
-      }
-    }
-    return {
-      labels: this.state.drawData.labels.reverse().slice(1).reverse(),
-      datasets: [
-        this.createDataForChart('Income Growth Rate', igr, brandDanger),
-      ]
-    }
-  }
-
   returnMember() {
-    if (this.state.drawData.total_member[0] === -1) return null;
+    if (this.state.drawData.total_members[0] === -1) return null;
     else {
       return (
           <Col sm={12} md className="mb-sm-2 mb-0">
-            <div className="text-value">{this.state.drawData.total_member.slice(-1)[0]}</div>
+            <div className="text-value">{this.state.drawData.total_members.slice(-1)[0]}</div>
             <div className="text-uppercase text-muted small">Members</div>
           </Col>
       )
@@ -208,9 +88,9 @@ class ProjectWidget extends Component {
                 <Col sm="7" className="d-none d-sm-inline-block">
                   <ButtonToolbar className="float-right" aria-label="Toolbar with button groups">
                     <ButtonGroup className="mr-3" aria-label="First group">
-                      <Button color="outline-secondary" onClick={() => this.fetchData('7')} active={this.state.days === '7'}>Day</Button>
-                      <Button color="outline-secondary" onClick={() => this.fetchData('30')} active={this.state.days === '30'}>Month</Button>
-                      <Button color="outline-secondary" onClick={() => this.fetchData('all')} active={this.state.days === 'all'}>All</Button>
+                      <Button color="outline-secondary" onClick={() => this.fetchData(this.props.id, '7')} active={this.state.days === '7'}>Week</Button>
+                      <Button color="outline-secondary" onClick={() => this.fetchData(this.props.id, '30')} active={this.state.days === '30'}>Month</Button>
+                      <Button color="outline-secondary" onClick={() => this.fetchData(this.props.id, 'all')} active={this.state.days === 'all'}>All</Button>
                     </ButtonGroup>
                   </ButtonToolbar>
                 </Col>
@@ -219,13 +99,15 @@ class ProjectWidget extends Component {
               <Row style={{ marginTop: 10 + 'px' }}>
                 <Col xs={12} sm={12} md={12} lg={6} xl={6}  >
                   <div className="chart-wrapper">
-                    <Line data={this.getPaidData()} options={options} />
+                    <Line data={dataWarehouse.getProfitData(this.state.drawData.total_investment,
+                      this.state.drawData.total_paid_out, this.state.drawData.labels)} options={dataWarehouse.optionsChart} />
                   </div>
                 </Col>
 
                 <Col  xs={12} sm={12} md={12} lg={6} xl={6}>
                   <div className="chart-wrapper">
-                    <Line data={this.getGrowthRateData()} options={options} />
+                    <Line data={dataWarehouse.getGrowthRateData(this.state.drawData.total_investment,
+                      this.state.drawData.total_paid_out, this.state.drawData.labels)} options={dataWarehouse.optionsChart} />
                   </div>
                 </Col>
               </Row>
@@ -242,9 +124,7 @@ class ProjectWidget extends Component {
                   <div className="text-uppercase text-muted small">Total Paid Out</div>
                 </Col>
 
-                
                 {this.returnMember()}
-                
 
                 <Col sm={12} md className="mb-sm-2 mb-0">
                   <div className="text-value">{this.state.drawData.alexa_rank.slice(-1)[0]}</div>
