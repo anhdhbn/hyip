e91202544fef2cb9ad6ea2a796afe94e32f009ab
Anh DH <anhdhbn@gmail.com>
2020-02-10 23:42:27 +0700

    frontend

--- frontend/src/services/requests.js
+++ frontend/src/services/requests.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 // export const BASE_API_URL = `${ 'http://backend/api/' || 'https://hyip-anhdh.herokuapp.com/api/'}`;
-export const BASE_API_URL = 'http://backend/api/';
+export const BASE_API_URL = 'http://anhdh.tk:5000/api/';
 
 const customAxios = axios.create({
   baseURL: BASE_API_URL,
