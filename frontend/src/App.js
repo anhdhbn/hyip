6238ab9c802ff5151863cc46cda530f3a468ddbf
Anh DH <anhdhbn@gmail.com>
2020-02-10 18:49:02 +0700

    finish this project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,9 +25,10 @@ class App extends Component {
               <Route exact path="/" render={() => (<Redirect to="/login"/>)}/>
               <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
               <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
+              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
               <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
               <Route path="/admin" name="Home" render={props => <DefaultLayout {...props}/>} />
-              <Route path='*' exact={true} name="Page 404" render={props => <Page404 {...props}/>} />
+              <Redirect from='*' to='/404' />
             </Switch>
             <ToastContainer/>
           </React.Suspense>
