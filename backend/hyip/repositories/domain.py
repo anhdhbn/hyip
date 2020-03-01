6ce5b352737f862cc92677cc100faca2843ca0a1
Anh DH <anhdhbn@gmail.com>
2020-03-01 04:17:21 -0800

    fix search domain

--- backend/hyip/repositories/domain.py
+++ backend/hyip/repositories/domain.py
@@ -6,6 +6,13 @@ __author__ = 'AnhDH'
 _logger = logging.getLogger(__name__)
 
 def search_domains(inputValue, page, ipp):
-    return models.Project.query.filter(
+    items = models.Project.query.filter(
         models.Domain.name.like(inputValue + '%')
     ).paginate(int(page), int(ipp), error_out=False).items
+
+    if len(items) == 0:
+        return models.Project.query.filter(
+        models.Domain.name.like('%' + inputValue + '%')
+    ).paginate(int(page), int(ipp), error_out=False).items
+    else:
+        return items
